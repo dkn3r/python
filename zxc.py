b4af00bd2cb67655651be7d11a372d0f3ac9b2e3
dkn3r <nazar.diachuk@ukr.net>
2022-03-31 16:08:53 +0300

    update

--- zxc.py
+++ zxc.py
@@ -1,17 +1,22 @@
 f1 = open('input.txt', 'r')
 f2 = open('output.txt', 'w')
-n = list(map(str, f1.readline()))
-rez = []
-a = 1
-for i in range(len(n)-1):
-    if i == len(n)-2:
-        rez.append(n[i])
-        rez.append(str(a+1))
-    else:
-        if n[i] == n[i+1]:
-            a += 1
-        else:
-            rez.append(n[i])
-            rez.append(str(a))
-            a = 1
-f2.write(''.join(rez))
+inp = list(map(int, f1.readline().split(', ')))
+key = True
+first = [inp[0]]
+del inp[0]
+second = 0
+while key:
+    if sum(first) == sum(inp):
+        first = list((map(str, first)))
+        inp = list((map(str, inp)))
+        f2.write('['+', '.join(first)+']')
+        f2.write(' ['+', '.join(inp)+']')
+        key = False
+        break
+
+    elif inp != sum(inp):
+        first.append(inp[0])
+        del inp[0]
+        if len(inp) == 0:
+            key = False
+            f2.write("False")
