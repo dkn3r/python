94dfe0822f0f6ffca488b33101b9a62ddee7047c
dkn3r <nazar.diachuk@ukr.net>
2022-04-03 21:55:37 +0300

    0.1

--- String decode.encode comparison/zxc.py
+++ String decode.encode comparison/zxc.py
@@ -7,28 +7,30 @@ del data1[0]
 del data1[-1]
 del data2[0]
 del data2[-1]
-data2 = ''.join(data2)
+rez2 = ''.join(data2)
+
 
 def func(data1):
     a = 1
-    rez_data1 = []
+    rez1 = []
     for i in range(len(data1)-1):
-
         if data1[i] == data1[i+1]:
             a += 1
-            print(a)
+            if i == len(data1)-2:
+                rez1.append(data1[i])
+                if a != 1:
+                    rez1.append(str(a))
         else:
-            rez_data1.append(data1[i])
+            rez1.append(data1[i])
             if a != 1:
-                rez_data1.append(str(a))
-                a = 1
-    
-    if data1[-2] != data1[-1]:
-        rez_data1.append(data1[-1])
-    print(rez_data1)
-
-    if ''.join(rez_data1) == data2:
+                rez1.append(str(a))
+            a = 1
+        if i == len(data1)-2 and data1[-1] != data1[-2]:
+            rez1.append(data1[-1])
+    if ''.join(rez1) == rez2:
         return True
     else:
         return False
+
+
 print(func(data1))
