6423263bbe7581636cf358ce105877e77d18a802
dkn3r <nazar.diachuk@ukr.net>
2022-04-05 10:14:52 +0300

    0.2

--- Balance arrays/zxc.py
+++ Balance arrays/zxc.py
@@ -1,27 +1,32 @@
 f1 = open('input.txt', 'r')
 
 read = list(map(int, f1.readline().split(', ')))  # зчитування чисел через кому
-
+f1.close()
 
 def func(read):
+    suma_read = sum(read)
+
     key = True
     first_part = []
+    suma_first_part = 0
+    
     while key:
-        # додавання в список first_element першого елемента read
+        
         first_part.append(read[0])
-        del read[0]                # і видалення з списку read
-        if sum(first_part) == sum(read):  # перевірка чи суми списків рівні
+        suma_first_part += read[0]
+        suma_read -= read[0]
+        read.pop(0)
+
+        if suma_first_part == suma_read: 
             key = False
             return first_part
-        if len(read) == 1:        # це якщо перевірка пройшла до останнього елементу і не знайшла рівні суми
+        if len(read) == 1:       
             key = False
             return False
 
-
 result = func(read)
-if result == False:  # це щоб не було квадратних дужок після False
+if not result:  
     print(result)
 else:
     print(result, read)
     print('True')
-f1.close()
