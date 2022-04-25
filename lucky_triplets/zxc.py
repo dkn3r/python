4fc39a8c698d4e5a624724c31711114a7cfb6168
dkn3r <nazar.diachuk@ukr.net>
2022-04-25 21:46:40 +0300

    0.1

--- lucky_triplets/zxc.py
+++ lucky_triplets/zxc.py
@@ -1,23 +1,15 @@
 f1 = open('input.txt', 'r')
 numbers = list(map(int, f1.readline().split(', ')))
 
-
-def all_the_same(numbers):
-    q = 0
-    for i in range(len(numbers)-1):
-        if numbers[i] == numbers[i+1]:
-            q += 1
-    if numbers[-1] == numbers[-2]:
-        q += 1
-    if q == len(numbers):
+'''def all_the_same(numbers):
+    if len(set(numbers)) == 1:
         return True
     else:
-        return False
+        return False'''
 
 
 def main(numbers):
     lucky_triplets = 0
-
     for first in range(len(numbers)):
         for second in range(first+1, len(numbers)):
             if numbers[second] % numbers[first] == 0:
@@ -27,6 +19,7 @@ def main(numbers):
     return lucky_triplets
 
 
-result1 = all_the_same(numbers)
+#result1 = all_the_same(numbers)
 result2 = main(numbers)
+
 print(result2)
