a54f6571920dc4a82deab400f60d818e8956dc09
dkn3r <nazar.diachuk@ukr.net>
2022-04-25 23:59:18 +0300

    0.1

--- #! lucky_triplets/zxc.py
+++ #! lucky_triplets/zxc.py
@@ -1,31 +1,30 @@
-import time
+#import time
 
 f1 = open('input.txt', 'r')
 numbers = list(map(int, f1.readline().split(', ')))
 f1.close()
 
 
-# --- 0.0009884834289550781 seconds ---
-''' this function is for all the same numbers in the list '''
+''' this function is for all the same numbers in the list 
+    --- 0.0009884834289550781 seconds ---'''
 
 
 def all_the_same(numbers):
     if len(set(numbers)) == 1:
         lucky_triplets = 0
         j = 0
-        lucky_triplets = et = ((len(numbers) - 2) *
-                               ((len(numbers) - 2) + 1)) // 2
-        while len(numbers) - 2 - j != 1:
+        n = len(numbers) - 2
+        lucky_triplets = (n * (n + 1)) // 2
+        while n - j != 1:
             j += 1
-            lucky_triplets += ((len(numbers) - 2 - j) *
-                               ((len(numbers)-2 - j) + 1)) // 2
+            lucky_triplets += ((n - j) * (n - j + 1)) // 2
         return lucky_triplets
     else:
         return False
 
 
-# --- 0.0010066032409667969 seconds ---
-''' this function is for all different numbers in the list '''
+''' this function is for all different numbers in the list
+    --- 0.0010066032409667969 seconds --- '''
 
 
 def main(numbers):
@@ -34,7 +33,7 @@ def main(numbers):
         for second in range(first+1, len(numbers)):
             if numbers[second] % numbers[first] == 0 and numbers[first] <= numbers[second]:
                 for third in range(second+1, len(numbers)):
-                    if numbers[third] % numbers[second] == 0 and numbers[first] <= numbers[second]:
+                    if numbers[third] % numbers[second] == 0 and numbers[second] <= numbers[third]:
                         lucky_triplets += 1
     return lucky_triplets
 # start_time = time.time()
